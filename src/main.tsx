e366d222f7196992f50f091f33e6f1c981f85b10
ALCEMIR HENRIQUE <henriquesantos1703m@gmail.com>
2026-03-09 23:05:57 +0000

    feat: adicionando o clarity

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Clarity
+(function (c: any, l: Document, a: string, r: string, i: string, t?: any, y?: any) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments)
+    }
+  t = l.createElement(r)
+  t.async = 1
+  t.src = 'https://www.clarity.ms/tag/' + i
+  y = l.getElementsByTagName(r)[0]
+  y.parentNode!.insertBefore(t, y)
+})(window, document, 'clarity', 'script', 'vtag7yal81')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
